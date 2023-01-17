1753a0ac0612239f075f08e4a04c1bcf4fbd6fdc
ATHARVA-MUJUMDAR <112259157+ATHARVA-MUJUMDAR@users.noreply.github.com>
2023-01-17 19:55:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+function handle(key)
+{
+   
+    
+    switch(key)
+    {
+        case "w":
+            var audio=new Audio("tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+                var audio=new Audio("tom-2.mp3");
+                audio.play();
+                break;
+        case "s":
+            var audio=new Audio("tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("crash.mp3");
+            audio.play();
+            break; 
+        case "l":
+            var audio=new Audio("kick-bass.mp3");
+            audio.play();
+            break;   
+    }
+}
+
+function animate(key)
+{
+    var active=document.querySelector("."+key);
+    active.classList.add("pressed");
+
+    setTimeout(function(){
+        active.classList.remove("pressed");},100);
+    
+}
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    var inner=this.innerHTML;
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var inner=this.innerHTML;
+        handle(inner);
+        animate(inner);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    handle(event.key);
+    animate(event.key);
+});
